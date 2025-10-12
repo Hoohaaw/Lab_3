92fbaafe0b1c9f6dafcb35d8d35f148c28170254
Alex Palm <ap224gy@student.lnu.se>
2025-10-12 21:25:22 +0200

    Add initiall router file

---
+++ public/routes/router.js
@@ -0,0 +1,24 @@
+import express from "express";
+import { userController } from "../../src/controllers/userController";
+
+export const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.render("index", { title: "Home" });
+});
+
+router.get("/login", (req, res) => {
+  res.render("login", { title: "Login" });
+});
+
+router.post("/login", (req, res) => {
+  userController.loginUser(req, res);
+});
+
+router.get("/register", (req, res) => {
+  res.render("register", { title: "Register" });
+});
+
+router.post("/register", (req, res) => {
+  userController.registerUser(req, res);
+});
