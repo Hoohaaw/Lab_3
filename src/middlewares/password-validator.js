81091f5244d74d8d1fe6cbf3f0408a2e178d6e49
Alex Palm <ap224gy@student.lnu.se>
2025-10-14 12:30:44 +0200

    Create initial password validator middleware

--- src/middlewares/password-validator.js
+++ src/middlewares/password-validator.js
@@ -1,3 +1,10 @@
 import passwordValidator from "password-validator-ap";
 
 const validator = new passwordValidator();
+
+function validatePassword(req, res, next, password, username) {
+  validator.validate(password, username);
+  next();
+}
+
+export default validatePassword;
