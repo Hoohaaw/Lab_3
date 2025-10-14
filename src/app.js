4d99ce241eb8a3da8743f0c33112b2ddf3a2184e
Alex Palm <ap224gy@student.lnu.se>
2025-10-14 12:39:21 +0200

    Remove ejs dependency

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
 import express from "express";
+import { connectToDatabase } from "./config/database.js";
+import { router } from "../public/routes/router.js";
 
 try {
+  await connectToDatabase(process.env.DB_CONNECTION_STRING);
   const app = express();
+
+  app.use("/", router);
+
 } catch (err) {
   console.error(err);
   process.exit(1);
