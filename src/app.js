6c15c2d728bff56277e8f1d03843899ef55f00c1
Alex Palm <ap224gy@student.lnu.se>
2025-10-16 20:39:19 +0200

    Add express static & views files

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
 import "dotenv/config";
 import express from "express";
+import { fileURLToPath } from "url";
 import { connectToDatabase } from "../public/config/mongoose.js";
 import { router } from "../public/routes/router.js";
 
 try {
-  const mongooseInstance = await connectToDatabase(process.env.DB_CONNECTION_STRING);
   const app = express();
+
+  // Serve static files
+  const publicDir = fileURLToPath(new URL("../public", import.meta.url));
+  app.use(express.static(publicDir));
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+
+  // View engine setup (EJS)
+  app.set("view engine", "ejs");
+  const viewsDir = fileURLToPath(new URL("../views", import.meta.url));
+  app.set("views", viewsDir);
 
   app.use("/", router);
 
