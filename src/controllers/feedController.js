022468d1db1278480b63863f90c4f4718b26e875
Alex Palm <ap224gy@student.lnu.se>
2025-10-17 19:09:52 +0200

    Improve feedController, not final version

--- src/controllers/feedController.js
+++ src/controllers/feedController.js
@@ -1,4 +1,4 @@
-import { Post } from "../models/Post.js";
+import Post from "../../public/models/postModel.js";
 class FeedController {
   async createPost(req, res) { // Create a new post
     const { username, content } = req.body;
@@ -9,11 +9,21 @@ class FeedController {
       res.status(400).send("Cant post an empty post");
     }
   }
-  async getPosts(req, res,) { // Get all posts
-    res.send("Get posts");
-    const posts = await Post.find();
+  async getPosts(req, res) {
+    try {
+      const fortyEightHoursAgo = new Date(Date.now() - 48 * 60 * 60 * 1000);
 
+      const posts = await Post.find({
+        createdAt: { $gte: fortyEightHoursAgo }
+      }).sort({ createdAt: -1 }); // newest first
+
+      res.status(200).json(posts);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Error fetching posts");
+    }
   }
+
   deletePost(req, res, next, id) { // delete a post
     res.send("Delete post");
     Post.findByIdAndDelete(id);
