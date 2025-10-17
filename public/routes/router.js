580a905a0ed3782f39af1980778fb491ed347028
Alex Palm <ap224gy@student.lnu.se>
2025-10-17 12:22:14 +0200

    Add feed route

--- public/routes/router.js
+++ public/routes/router.js
@@ -7,6 +7,10 @@ router.get("/", (req, res) => {
   res.render("feed", { title: "Feed" });
 });
 
+router.get("/feed", (req, res) => {
+  res.render("feed", { title: "Feed" });
+});
+
 router.get("/login", (req, res) => {
   res.render("login", { title: "Login" });
 });
@@ -19,6 +23,10 @@ router.get("/register", (req, res) => {
   res.render("register", { title: "Register" });
 });
 
-router.post("/register", (req, res) => {
-  userController.registerUser(req, res);
+router.post("/register", async (req, res) => {
+  try {
+    await userController.registerUser(req, res);
+  } catch (error) {
+    res.status(500).send("Error registering user", error.message);
+  }
 });
