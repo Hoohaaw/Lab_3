e8f3c8491fc2d3d338ecf812d1ddff8043a208de
Alex Palm <ap224gy@student.lnu.se>
2025-10-17 19:09:03 +0200

    Improve userController, not final version

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,16 +4,52 @@ import User from "../../public/models/userModel.js";
 const validator = new passwordValidator();
 
 class UserController {
-  loginUser(req, res) {
-    res.send("Login user");
-    const { username, password } = req.body;
-    validator.validate(password, username);
+  async loginUser(req, res) {
+    try {
+      const { username, password } = req.body;
+      console.log("Login attempt", username); // testing
+
+      const user = await User.findOne({ username, password });
+
+      if (user) {
+        console.log("Login successful for user:", username); // testing
+        res.status(200).send("Login successful");
+      } else {
+        console.log("Login failed for user:", username); // testing
+        res.status(401).send("Invalid username or password");
+      }
+    } catch (error) {
+      console.error("Error during login:", error);
+      res.status(500).send("Internal server error");
+    }
+
   }
+
   async registerUser(req, res) {
-    res.send("Register user");
-    await User.create({ username: req.body.username, password: req.body.password });
+    try {
+      const { username, password } = req.body;
+      console.log("register attempt: ", username); // testing
+
+      const isValid = validator.validate(password, username);
+      console.log("Password validation result:", isValid); // testing
+
+      const existingUser = await User.findOne({ username });
+      if (existingUser) {
+        console.log("Registration failed: username already taken"); // testing
+        return res.status(400).send("Username already exists");
+      }
+
+      const newUser = await User.create({ username, password});
+      console.log("New user created:", newUser); // testing
+      await newUser.save();
+      res.status(201).send("User registered successfully");
+    } catch {
+      res.status(400).send("Could not register user");
+    }
+
   }
-  deleteUser(req, res) {
+
+  async deleteUser(req, res) {
     res.send("Delete user");
   }
 
