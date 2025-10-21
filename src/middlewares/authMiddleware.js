08aeaa0c695ad2e497d2ecbe30c2b8adbe8c6bfa
Alex Palm <ap224gy@student.lnu.se>
2025-10-21 18:40:37 +0200

    Add redirect if authenticated method

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -18,3 +18,21 @@ export function requireAuth(req, res, next) {
     res.redirect("/login");
   }
 }
+
+export function redirectIfAuth(req, res, next) {
+  const token = req.cookies.authToken;
+
+  if (token) {
+    try {
+      jwt.verify(token, JWT_SECRET);
+      // Token is valid, redirect to feed
+      return res.redirect("/");
+    } catch (err) {
+      // Token is invalid, continue to login/register
+      next();
+    }
+  } else {
+    // No token, continue to login/register
+    next();
+  }
+}
