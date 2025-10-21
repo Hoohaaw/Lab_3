d29101f35006a8d0372bdfaf66f968a505364694
Alex Palm <ap224gy@student.lnu.se>
2025-10-21 12:24:02 +0200

    Update /create route to use correct function

--- public/routes/feedRoutes.js
+++ public/routes/feedRoutes.js
@@ -4,7 +4,7 @@ import { requireAuth } from "../../src/middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/create", requireAuth, (req, res) => feedController.createPost(req, res));
+router.post("/create", requireAuth, (req, res) => feedController.handleCreatePost(req, res));
 router.get("/", requireAuth, (req, res) => feedController.getPosts(req, res));
 router.get("/posts", requireAuth, (req, res) => feedController.getPosts(req, res));
 router.post("/like", requireAuth, (req, res) => feedController.updateLikeCount(req, res));
